646249d417ee7caf60786b2209f38f0a3bd23870
Tudor Lapuste <tlapuste@gmail.com>
2020-03-03 21:44:28 -0800

    fix header

--- src/main/webapp/app/shared/layout/header/header.tsx
+++ src/main/webapp/app/shared/layout/header/header.tsx
@@ -42,11 +42,10 @@ const Header = (props: IHeaderProps) => {
         <Brand />
         <Collapse isOpen={menuOpen} navbar>
           <Nav id="header-tabs" className="ml-auto" navbar>
-            {/*<Home />*/}
-            {props.isAuthenticated && props.isAdmin && <EntitiesMenu />}
-            {props.isAuthenticated && props.isAdmin && (
-              <AdminMenu showSwagger={props.isSwaggerEnabled} showDatabase={!props.isInProduction} />
-            )}
+            { props.isAuthenticated && props.isAdmin && <>
+                <EntitiesMenu />
+                <AdminMenu showSwagger={props.isSwaggerEnabled} showDatabase={!props.isInProduction} />
+            </>}
             <AccountMenu isAuthenticated={props.isAuthenticated} />
           </Nav>
         </Collapse>
